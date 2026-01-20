0a4ef9db405c70eaaf8d9cccddc81a7911112936
Lucas <12496191+lucashuy@users.noreply.github.com>
2026-01-19 22:18:16 -0800

    Solve Site Ground captcha

--- retailers/src/retailers/html/g4c_gun_store.rs
+++ retailers/src/retailers/html/g4c_gun_store.rs
@@ -1,24 +1,50 @@
+use std::time::Instant;
+
 use async_trait::async_trait;
+use base64::{Engine, prelude::BASE64_STANDARD};
 use common::result::{
     base::CrawlResult,
     enums::{Category, RetailerName},
 };
-use crawler::request::{Request, RequestBuilder};
+use crawler::{
+    request::{Request, RequestBuilder},
+    unprotected::UnprotectedCrawler,
+};
+use regex::Regex;
 use scraper::{ElementRef, Html, Selector};
-use tracing::debug;
+use sha1::{Digest, Sha1};
+use tracing::{debug, info};
 
 use crate::{
     errors::RetailerError,
     structures::{HtmlRetailer, HtmlRetailerSuper, HtmlSearchQuery, Retailer},
     utils::{
         ecommerce::{WooCommerce, WooCommerceBuilder},
-        html::extract_element_from_element,
+        html::{element_extract_attr, extract_element_from_element},
+        regex::unwrap_regex_capture,
     },
 };
 
 const MAX_PER_PAGE: &str = "48";
+const BASE_URL: &str = "https://g4cgunstore.com";
 const URL: &str =
     "https://g4cgunstore.com/product-category/{category}/page/{page}/?per_page={max_per_page}";
+
+#[derive(Debug)]
+struct ChallengeSolution {
+    matching_hash: String,
+    elapsed_time: u64,
+    counter: u32,
+}
+
+impl ChallengeSolution {
+    fn get_submit_url(&self) -> String {
+        format!(
+            "https://g4cgunstore.com/.well-known/sgcaptcha/?r=%2F&sol={}%3D%3D&s={}:{}",
+            self.matching_hash, self.elapsed_time, self.counter
+        )
+    }
+}
 
 pub struct G4CGunStore;
 
@@ -45,11 +71,158 @@ impl G4CGunStore {
         )
         .is_ok();
     }
+
+    fn extract_sg_redirect(html: &str) -> Result<String, RetailerError> {
+        // this is just a redirct to the well-known URI
+        // technically not needed since all I need is the IP
+        // but they provide it, so why not
+
+        let html = Html::parse_document(html);
+        let meta = extract_element_from_element(html.root_element(), "meta")?;
+        let content = element_extract_attr(meta, "content")?;
+
+        let Some((_, uri)) = content.split_once(";") else {
+            return Err(RetailerError::GeneralError(
+                "Invalid meta; expected content to contain semicolon".to_string(),
+            ));
+        };
+
+        Ok(format!("{BASE_URL}{uri}"))
+    }
+
+    async fn get_captcha_challenge(url: &str) -> Result<String, RetailerError> {
+        let regex =
+            Regex::new(r#"const\s+sgchallenge\s*=\s*"([^"]+)""#).expect("Static regex to compile");
+
+        let request = RequestBuilder::new().set_url(url).build();
+        let result = UnprotectedCrawler::make_web_request(request).await?;
+
+        let challenge = unwrap_regex_capture(&regex, &result.body)?;
+
+        Ok(challenge)
+    }
+
+    fn get_difficulty(challenge_string: &str) -> Result<u32, RetailerError> {
+        let Some((difficulty, _)) = challenge_string.split_once(":") else {
+            return Err(RetailerError::GeneralError(format!(
+                "Expected challenge string to contain colon, instead got: \"{challenge_string}\""
+            )));
+        };
+
+        let Ok(difficulty_int) = difficulty.parse::<u32>() else {
+            return Err(RetailerError::GeneralError(format!(
+                "Invalid difficulty, expected int, got: \"{difficulty}\""
+            )));
+        };
+
+        if difficulty_int > 32 {
+            return Err(RetailerError::GeneralError(format!(
+                "Invalid difficulty, expected int < 32, got: \"{difficulty_int}\""
+            )));
+        }
+
+        Ok(difficulty_int)
+    }
+
+    fn convert_counter(counter: u32) -> Vec<u8> {
+        let word = counter.to_be_bytes();
+
+        // u32 will always result in 4 byte array
+        // meaning defaulting to `3` will pull `0`
+        // in the case of first counter
+        let position = word.iter().position(|&byte| byte != 0).unwrap_or(3);
+
+        word[position..].to_vec()
+    }
+
+    fn validate_hash(hash: &[u8; 20], difficulty: u32) -> bool {
+        // this assumes that Site Ground doesn't do something dumb
+        // and set the difficulty to something more than 32 bits
+        let first_32_bits = u32::from_be_bytes(
+            hash[..4]
+                .try_into()
+                .expect("SHA1 result to always be 20 bytes"),
+        );
+
+        first_32_bits.leading_zeros() >= difficulty
+    }
+
+    fn solve_challenge(challenge_string: &str) -> Result<ChallengeSolution, RetailerError> {
+        let start_time = Instant::now();
+
+        let difficulty = Self::get_difficulty(challenge_string)?;
+        let nonce_bytes = challenge_string.as_bytes();
+
+        let mut counter: u32 = 0;
+
+        let mut hasher = Sha1::new();
+        let mut pow_input: Vec<u8> = Vec::new();
+        pow_input.extend(nonce_bytes);
+
+        info!("Begin proof of work");
+
+        // max iterations should be 2^(difficulty - 1)
+        // so basically 20 should be max of 1 million-ish
+        // this will fail if they give us difficulty > 32
+        while counter < (2 << difficulty - 1) {
+            let big_endian_counter = Self::convert_counter(counter);
+
+            // delete counter and re-append
+            pow_input.truncate(nonce_bytes.len());
+            pow_input.extend(big_endian_counter);
+
+            hasher.update(&pow_input);
+
+            let hash: [u8; 20] = hasher.finalize_reset().into();
+
+            counter += 1;
+
+            if Self::validate_hash(&hash, difficulty) {
+                return Ok(ChallengeSolution {
+                    matching_hash: BASE64_STANDARD.encode(&pow_input),
+                    // as_millis() returns u128, I really hope that
+                    // I don't need 16 full bytes for this
+                    elapsed_time: start_time.elapsed().as_millis() as u64,
+                    counter: counter,
+                });
+            }
+        }
+
+        Err(RetailerError::GeneralError(
+            "Failed to find solution".to_string(),
+        ))
+    }
 }
 
 impl HtmlRetailerSuper for G4CGunStore {}
 
+#[async_trait]
 impl Retailer for G4CGunStore {
+    async fn init(&mut self) -> Result<(), RetailerError> {
+        let request = RequestBuilder::new().set_url(BASE_URL).build();
+        let result = UnprotectedCrawler::make_web_request(request).await?;
+
+        if result.body.contains("/.well-known/sgcaptcha/") {
+            // siteground really expects a client sided check to fix bots
+            // from visiting the site, okay
+
+            info!("Solving Site Ground challenge");
+
+            let redirect = Self::extract_sg_redirect(&result.body)?;
+            let challenge = Self::get_captcha_challenge(&redirect).await?;
+
+            info!("Extracted challenge string: \"{challenge}\"");
+
+            let solution = Self::solve_challenge(&challenge)?;
+            let request = RequestBuilder::new()
+                .set_url(solution.get_submit_url())
+                .build();
+            let _ = UnprotectedCrawler::make_web_request(request).await?;
+        }
+
+        Ok(())
+    }
+
     fn get_retailer_name(&self) -> RetailerName {
         RetailerName::G4CGunStore
     }
