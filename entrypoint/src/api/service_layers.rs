cc4474e63cb1572f960bfc2fd183b65918eff271
Lucas <12496191+lucashuy@users.noreply.github.com>
2026-02-10 20:56:09 -0800

    Add default timeout to incoming API requests

--- entrypoint/src/api/service_layers.rs
+++ entrypoint/src/api/service_layers.rs
@@ -1,12 +1,22 @@
-use axum::http::{HeaderValue, Method};
-use reqwest::header::CONTENT_TYPE;
+use std::time::Duration;
+
+use axum::http::{HeaderValue, Method, StatusCode, header::CONTENT_TYPE};
 use tower::{
     ServiceBuilder,
     layer::util::{Identity, Stack},
 };
-use tower_http::cors::CorsLayer;
+use tower_http::{cors::CorsLayer, timeout::TimeoutLayer};
 
-pub(crate) fn build_service_layers() -> ServiceBuilder<Stack<CorsLayer, Identity>> {
+const DEFAULT_TIMEOUT: u64 = 5;
+
+pub(crate) fn build_service_layers()
+-> ServiceBuilder<Stack<TimeoutLayer, Stack<CorsLayer, Identity>>> {
+    ServiceBuilder::new()
+        .layer(build_cors())
+        .layer(build_timeout())
+}
+
+fn build_cors() -> CorsLayer {
     let mut cors_layer = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
         .allow_headers([CONTENT_TYPE]);
@@ -16,5 +26,12 @@ pub(crate) fn build_service_layers() -> ServiceBuilder<Stack<CorsLayer, Identity
             cors_layer.allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap());
     }
 
-    ServiceBuilder::new().layer(cors_layer)
+    cors_layer
+}
+
+fn build_timeout() -> TimeoutLayer {
+    TimeoutLayer::with_status_code(
+        StatusCode::GATEWAY_TIMEOUT,
+        Duration::from_secs(DEFAULT_TIMEOUT),
+    )
 }
