b0aa9ffc035b4693ffbbd84c7856fbd9bbd3338d
Lucas <12496191+lucashuy@users.noreply.github.com>
2026-03-19 21:08:19 -0700

    Update Canfirearm naming behaviour

--- retailers/src/retailers/html/canfirearm.rs
+++ retailers/src/retailers/html/canfirearm.rs
@@ -1,11 +1,14 @@
+use std::sync::LazyLock;
+
 use async_trait::async_trait;
 use common::result::{
     base::CrawlResult,
     enums::{Category, RetailerName},
 };
 use crawler::request::{Request, RequestBuilder};
+use regex::Regex;
 use scraper::{Html, Selector};
-use tracing::debug;
+use tracing::{debug, warn};
 
 use crate::{
     errors::RetailerError,
@@ -13,8 +16,13 @@ use crate::{
     utils::{
         ecommerce::{WooCommerce, WooCommerceBuilder},
         html::{element_to_text, extract_element_from_element},
+        regex::unwrap_regex_capture,
     },
 };
+
+static REGEX: LazyLock<Regex> = LazyLock::new(|| {
+    Regex::new(r"(?i)^BONUS WITH PURCHASE.*\s{2,}(.*)").expect("Regex to not change")
+});
 
 const URL: &str =
     "https://canfirearm.com/product-category/{category}/page/{page}/?stock_status=instock";
@@ -89,11 +97,22 @@ impl HtmlRetailer for CanFirearm {
                 continue;
             }
 
-            results.push(woocommerce.parse_product(
-                element,
-                self.get_retailer_name(),
-                search_term.category,
-            )?);
+            // this is the first retailer I've seen that has an invalid product
+            // image on their listings, and the actual image is a PDF of image
+            // attachment in an email, just ignore products in this case
+            match woocommerce.parse_product(element, self.get_retailer_name(), search_term.category)
+            {
+                Ok(mut product) => {
+                    // remove the dumb "bonus with purchase" text they have
+                    // if you want to discount the product, list it on sale
+                    if let Ok(clean_name) = unwrap_regex_capture(&REGEX, &product.name) {
+                        product.update_name(&clean_name);
+                    }
+
+                    results.push(product);
+                }
+                Err(err) => warn!("Invalid product parsed, ignoring: {err}"),
+            };
         }
 
         Ok(results)
