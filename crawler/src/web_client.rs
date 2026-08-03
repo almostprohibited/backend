a6dc32a79a5d8d685ad09282f3a7ff9aa7589d6a
Lucas <12496191+lucashuy@users.noreply.github.com>
2026-08-02 23:48:08 -0700

    Add HTTP signature to all requests

--- crawler/src/web_client.rs
+++ crawler/src/web_client.rs
@@ -1,5 +1,6 @@
 use std::{str::FromStr, sync::OnceLock};
 
+use common::http_sig::create_request_headers;
 use reqwest::{
     StatusCode,
     header::{HeaderMap, HeaderName, HeaderValue},
@@ -58,6 +59,14 @@ impl WebClient {
             }
 
             request_builder = request_builder.headers(header_map);
+        }
+
+        let sig_headers = create_request_headers(&request.url);
+        request_builder = request_builder.header("Signature", sig_headers.signature);
+        request_builder = request_builder.header("Signature-Input", sig_headers.signature_input);
+
+        if let Some(sig_agent) = sig_headers.signature_agent {
+            request_builder = request_builder.header("Signature-Agent", sig_agent);
         }
 
         let response = request_builder.send().await?;
